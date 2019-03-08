0e613cbb9e30235dbb76c60e437908f0c2b8b345
Peter Paul <peterpaularasu@gmail.com>
2019-03-08 22:25:15 +0530

    swrweal done. tab bar done.

--- christian_spot/christian_spot/controller/ParentVC.swift
+++ christian_spot/christian_spot/controller/ParentVC.swift
@@ -7,20 +7,39 @@
 //
 
 import UIKit
-
-class ParentVC: UIViewController {
+import XLPagerTabStrip
+class ParentVC: ButtonBarPagerTabStripViewController{
 
     @IBOutlet weak var revealMenu: UIButton!
-    
+    let wcolor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
     override func viewDidLoad() {
+        //tab
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = wcolor
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            oldCell?.label.textColor = .black
+            newCell?.label.textColor = self?.wcolor
+        }
         super.viewDidLoad()
+       //reveal
         revealMenu.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
-    self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-    self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
-       
+    
         
     }
-
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let child_1 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "favourites")
+        let child_2 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "songs")
+        return [child_1, child_2]
+    }
 
 }
 
--- christian_spot/christian_spot/controller/favouritesVC.swift
+++ christian_spot/christian_spot/controller/favouritesVC.swift
@@ -7,24 +7,14 @@
 //
 
 import UIKit
-
-class favouritesVC: UIViewController {
+import XLPagerTabStrip
+class favouritesVC: UIViewController, IndicatorInfoProvider {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "favourites")
     }
-    */
-
 }
